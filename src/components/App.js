0a2f6458e64519cb4f4e2ec6299c6dfcd700bbba
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-04-25 16:19:46 +0530

    done2

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,36 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-import Child1 from "./Child1";
-import Child2 from "./Child2";
+
 
 const App = () => {
   const [selectedOption,setSelectedOption] = useState("");
   return (
-    <div>
+    <div className="parent">
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
         <Child1  setSelectedOption={setSelectedOption}/>
         <Child2 setSelectedOption={setSelectedOption}/>
         <p>Selcted Option: {selectedOption}</p>
     </div>
+  )
+}
+
+const Child1 = ({setSelectedOption}) => {
+  return (
+      <div>
+          <h1>Child Component 1</h1>
+          <button onClick={()=>{setSelectedOption("Option 1")}}>Option 1</button>
+      </div>
+  )
+}
+
+const Child2 = ({setSelectedOption}) => {
+  return (
+      <div>
+          <h1>Child Component 2</h1>
+          <button onClick={()=>{setSelectedOption("Option 2")}}>Option 2</button>
+      </div>
   )
 }
 
